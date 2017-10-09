ae081b0f1c1f3dfda275076b68a2a2d47732fe32
Van Dang <Van Dang>
2017-10-08 23:53:06 -0500

    psuedocode and basic layout

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,41 +1,64 @@
 $(document).ready(function() {
 
-var score = 0;
-var questionNum = 1;
+var correct;
+var incorrect;
+var unanswered;
+var questionNum = 0;
 var time = 30;
 var trivia = {
-	questions: ["q1","q2","q3"],
-	q1: ["a1","b1","c1","d1"], 
-	q2: ["a2","b2","c2","d2"],
-	q3: ["a3","b3","c3","d3"]
+	"question 1": ["a1","b1","c1","d1"], 
+	"question 2": ["a2","b2","c2","d2"],
+	"question 3": ["a3","b3","c3","d3"]
 };
 
+var questions = Object.keys(trivia);
+
+var correct = ["a1","b3","c2"]; //maybe true false for each question?
+
 function initialize() {
-	questionNum = 1;
+	questionNum = 0;
 	score = 0;
-	//insert question1 + answers
+	newQuestion();
 };
 
 function newQuestion() {
-	$("#question").text(trivia.questions[questionNum]);
+	//test purposes -- real version should append into answers div
+	//maybe when appending answers, give attribute of true or false
+	//don't forget to give .choice and #choice0 class/id
+	var q = questions[questionNum];
+	$("#question").text(q);
+	for (var i = 0; i < questions.length + 1; i++)
+		$("#choice"+i).text(trivia[q][i]);
 	questionNum++;
+	console.log(questionNum);
 };
 
 function endGame() {
-	//your score is [score] of [questions.length + 1]
+	//show correct, incorrect, and unanswered
+	//clear answers div
+	//start over button
 };
 
-$(".choice").on("click", function() {
+$("button").on("click", function() {
 	newQuestion();
-	// if correct answer, score++
-	if (questionNum > trivia.questions.length) {
+	// if correct answer, correct++
+	// display congrats/fail screen with right answer
+	if (questionNum >= questions.length) {
 		endGame();
 	}
-});
+}); // choice click
+
+
+function test() {
+	$("#options").html(""); //clears answers div... replace with gif and correct answer and stuff
+}
 
 
 
+}); // document ready 
 
 
+// start button
+// blank div for answers which get appended when game starts (with class attribute .choice and classes #choice1 etc)
+// when answer is clicked/end game, clear answers div
 
-});
