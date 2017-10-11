e28d645a07e8f87f4bae0b227ce55ed32116a19f
Van Dang <Van Dang>
2017-10-10 21:16:21 -0500

    Added theme

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,32 +5,86 @@ var incorrect;
 var unanswered;
 var questionNum = 0;
 var time = 30;
-var trivia = {
-	"question 1": ["a1","b1","c1","d1"], 
-	"question 2": ["a2","b2","c2","d2"],
+var countdown;
+var trivia = { //maybe make the correct answer the 5th "answer" and if chosen = index[4] then correct
+	"What is Sen's real name in Spirited Away?": ["Rika","Chihiro","Ana","Kumiko"], 
+	"In which movie can you ride the Cat Bus?": ["Howl's Moving Castle","Spirited Away","Castle in the Sky","My Neighbor Totoro"],
 	"question 3": ["a3","b3","c3","d3"]
 };
 
+// use API from slack channel!!!!!!!!1
+
+
 var questions = Object.keys(trivia);
 
-var correct = ["a1","b3","c2"]; //maybe true false for each question?
+var rightAnswer = ["Chihiro","My Neighbor Totoro","c2"]; //maybe true false for each question?
 
 function initialize() {
 	questionNum = 0;
 	score = 0;
-	newQuestion();
+	correct = 0;
+	incorrect = 0;
+	time = 30;
 };
 
+function clearButtons() {
+	$("#options").html(""); //clears answers div... replace with gif and correct answer and stuff
+}
+
 function newQuestion() {
-	//test purposes -- real version should append into answers div
 	//maybe when appending answers, give attribute of true or false
-	//don't forget to give .choice and #choice0 class/id
+	// newTime();
 	var q = questions[questionNum];
 	$("#question").text(q);
-	for (var i = 0; i < questions.length + 1; i++)
+	clearButtons();
+	for (var i = 0; i < questions.length + 1; i++) {
+		$("#options").append("<button class='choice' id='choice"+i+"' />");
 		$("#choice"+i).text(trivia[q][i]);
+		console.log(trivia[q][i]);
+	};
 	questionNum++;
 	console.log(questionNum);
+};
+
+// function newQuestion() {
+// 	newTime();
+// 	for (var i = 0; i < questions.length + 1; i++) {
+// 		var btn = $("<button>");
+// 		btn.addClass("choice");
+// 		btn.addId("choice"+i);// 
+// 		btn.attr("answerVal", trivia[q][i]);
+// 		btn.text(trivia[q][i]);
+// 	    $("#choice"+i).append(btn);
+// 	};
+// 	questionNum++;
+// 	console.log(questionNum);
+// };
+
+
+		//do this when creating buttons to be able to check value of button (to compare to correct)
+         //https://stackoverflow.com/questions/487056/retrieve-button-value-with-jquery 
+
+      //     function alertMovieName() {
+
+      //   // YOUR CODE GOES HERE!!!
+      //   console.log($(this).attr("data-name"));
+      // }
+
+
+
+function startTime() { //????????????
+	countdown = setInterval()
+	$("#time").html("<p>Time remaining: 30 seconds</p>") //ughhhh
+};
+
+function answerPage() {
+	// if answer right/wrong, display Nope or Correct or Out of Time
+	// if correct answer, correct++
+	// display congrats/fail screen with right answer
+	// if (time == 0) {
+	// 	unanswered++;
+	// }
+	setTimeout(newQuestion, 5000); //doesn't show next question until 5 secs
 };
 
 function endGame() {
@@ -39,26 +93,19 @@ function endGame() {
 	//start over button
 };
 
-$("button").on("click", function() {
+$("#startBtn").on("click", function() {
 	newQuestion();
-	// if correct answer, correct++
-	// display congrats/fail screen with right answer
-	if (questionNum >= questions.length) {
-		endGame();
-	}
-}); // choice click
+});
 
-
-function test() {
-	$("#options").html(""); //clears answers div... replace with gif and correct answer and stuff
-}
+$(".choice").on("click", function() {
+	// answerPage();
+	// if (questionNum >= questions.length) {
+	// 	endGame();
+	// }
+	console.log("choice click works!");
+}); // close button click
 
 
 
 }); // document ready 
-
-
-// start button
-// blank div for answers which get appended when game starts (with class attribute .choice and classes #choice1 etc)
-// when answer is clicked/end game, clear answers div
 
